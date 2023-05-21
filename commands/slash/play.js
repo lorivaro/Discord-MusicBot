8ebdc0c6ec980e71158d2ecca8b69770e72d1f7a
lorivaro <loukrijnders@gmail.com>
2023-05-21 22:26:30 +0200

    keep play messages

--- commands/slash/play.js
+++ commands/slash/play.js
@@ -183,8 +183,8 @@ const command = new SlashCommand()
       await interaction.editReply({ embeds: [playlistEmbed] }).catch(this.warn);
     }
 
-    if (ret) setTimeout(() => ret.delete().catch(this.warn), 20000);
-    return ret;
+    // if (ret) setTimeout(() => ret.delete().catch(this.warn), 20000);
+    // return ret;
   });
 
 module.exports = command;
